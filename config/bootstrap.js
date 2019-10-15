fb16a759a96b63f212ad43c435b30940da4f825c
辛小宝 <xxb@xinxiaobaodeMacBook-Pro.local>
2019-10-15 16:27:42 +0800

    add initial data

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -24,11 +24,11 @@ module.exports.bootstrap = async function() {
    }
   
    await House.createEach([
-     { title:"沙田第一城", ChineseName:"", name:"City One Shatin", gross_are:11, rent:11, URL :"/images/pic1.jpg",bedrooms:1, tenants:1,},
-     { title:"大围名城", ChineseName:"", name:"Tai Wai", gross_are:22, rent:22, URL :"/images/pic2.jpg",bedrooms:2, tenants:2,},  
-     { title:"黄埔花园",ChineseName:"", name:"Hung Hom", gross_are:33, rent:33, URL :"/images/pic3.jpeg",bedrooms:3, tenants:3,},  // ```
+     { title:"沙田第一城", ChineseName:"", name:"City One Shatin", gross_area:11, rent:11, URL :"/images/pic1.jpg",bedrooms:1, tenants:1,},
+     { title:"大围名城", ChineseName:"", name:"Tai Wai", gross_area:22, rent:22, URL :"/images/pic2.jpg",bedrooms:2, tenants:2,},  
+     { title:"黄埔花园",ChineseName:"", name:"Hung Hom", gross_area:33, rent:33, URL :"/images/pic3.jpeg",bedrooms:3, tenants:3,},  // ```
      
-     { title:"蓝湾半岛",ChineseName:"", name:"Siu Sai Wan", gross_are:44, rent:44, URL :"/images/pic4.jpg",bedrooms:4, tenants:4,},
+     { title:"蓝湾半岛",ChineseName:"", name:"Siu Sai Wan", gross_area:44, rent:44, URL :"/images/pic4.jpg",bedrooms:4, tenants:4,},
     ]);
 
      // // Set up fake development data (or if we already have some, avast)
